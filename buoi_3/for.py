500e36ed0946ba90560315f4756c053d26ae723a
CuongGM <cuonggm48@gmail.com>
2025-09-18 15:01:31 +0700

    fix

--- buoi_3/for.py
+++ buoi_3/for.py
@@ -1,10 +1,7 @@
 # 2 kiểu lặp: lặp với số lần biết trước, lặp với số lần không biết trước
 
 for i in range(1, 10, 2): # 1,3,5,7,9
-  print(i) 
-  fdsf
-  fdsfds
-  fdsf         # In ra i
+  print(i)    # In ra i
 
 # Đếm đến khi mỏi mồm thì thôi
 moi_mom = False # van chua moi mom
